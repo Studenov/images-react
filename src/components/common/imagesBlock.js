b2768b4f50d79c087614c9f7fc83124c061867fd
Roman <studenovroman@yandex.ru>
2018-06-13 12:59:00 +0600

    update component

--- src/components/common/imagesBlock.js
+++ src/components/common/imagesBlock.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default(props) => (
     <div className='img_block'>
         <div className='img_block_div'>
-            <p className='img_block_p'>{props.title}</p> 
-            <button onClick={props.delImages} className='img_block_button'>Удалить</button>
+            <div className='img_block_left'>
+                <p className='img_block_p'>{props.title}</p>
+            </div>
+            <div className='img_block_right'> 
+                <button onClick={props.delImages} className='img_block_button'>Удалить</button>
+            </div>
         </div>
         <img src={props.src} className='img_block_images'/>
     </div>
